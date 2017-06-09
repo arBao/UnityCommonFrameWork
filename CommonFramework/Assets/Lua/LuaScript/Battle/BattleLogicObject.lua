6ef99eba2e46e08b3940f0521b8f307232ba56ce
arBao <286075720@qq.com>
2017-06-09 13:47:59 +0800

    战斗开发

---
+++ CommonFramework/Assets/Lua/LuaScript/Battle/BattleLogicObject.lua
@@ -0,0 +1,25 @@
+---
+--- Created by luzhuqiu.
+--- DateTime: 2017/6/9 下午12:01
+---
+BattleLogicObject = class()
+
+function BattleLogicObject:ctor()
+    self.ID = ''
+end
+
+function BattleLogicObject:SetImage(data)
+
+end
+
+function BattleLogicObject:GetImage()
+
+end
+
+function BattleLogicObject:ReUse()
+
+end
+
+function BattleLogicObject:UnUse()
+
+end
---
+++ CommonFramework/Assets/Lua/LuaScript/Battle/BattleRenderObject.lua
@@ -0,0 +1,20 @@
+---
+--- Created by luzhuqiu.
+--- DateTime: 2017/6/9 下午12:24
+---
+BattleRenderObject = class()
+function BattleRenderObject:ctor()
+    self.ID = ''
+end
+
+function BattleRenderObject:Render()
+
+end
+
+function BattleRenderObject:ReUse()
+
+end
+
+function BattleRenderObject:UnUse()
+
+end
